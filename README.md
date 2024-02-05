78f73c0d5a97d5e142b5bef1bed09facf041c0c1
Rebeca <rebecacarolina75@gmail.com>
2024-02-05 15:00:06 -0300

    DataBase

--- README.md
+++
@@ -1,6 +0,0 @@
-# Sprint Ι
-
-Repositório com os conteúdos desenvolvidos nas aulas de Banco De Dados
-- Banco de Dados 
-  - Modelagem
-  - Scripts 
--- exercícios-scripts/1.1/DQL.sql
+++
@@ -1,7 +0,0 @@
-USE Exercicio_1_1;
-
-SELECT Pessoa.Nome, Pessoa.CNH, Telefone.IdTelefone, Email.IdEmail
-FROM Pessoa
-RIGHT JOIN Telefone ON Telefone.IdPessoa = Pessoa.IdPessoa
-RIGHT JOIN Email ON Email.IdPessoa = Pessoa.IdPessoa
-ORDER BY Pessoa.Nome DESC;
